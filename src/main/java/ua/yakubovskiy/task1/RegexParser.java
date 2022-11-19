3fe356e62fec4740ea9e130617daf50234fc51dd
Gamer <nelo9997@gmai.com>
2022-11-20 00:01:42 +0200

    version 11/19/22

--- src/main/java/ua/yakubovskiy/task1/RegexParser.java
+++ src/main/java/ua/yakubovskiy/task1/RegexParser.java
@@ -23,26 +23,46 @@ public class RegexParser {
              BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
             while (reader.ready()) {
                 String testHtml = reader.readLine();
-                strings.append(testHtml);
-                if(testHtml.contains("/>")){
-                    Pattern attValueDoubleQuoteOnly = Pattern.compile("([\\w:\\-]+)(\\s*=\\s*(\"(.*?)\"))");
-                    Matcher m = attValueDoubleQuoteOnly.matcher(strings.toString());
+                strings.append(testHtml).append("\n");
+                if(testHtml.contains("/")){
+                    Matcher matcher = Pattern.compile("([\\w:\\-]+)(\\s*=\\s*(\"(.*?)\"))")
+                            .matcher(strings.toString());
                     String name = "";
                     String surname = "";
-                    while (m.find()) {
-                        if(m.group(0).startsWith("n")) {
-                            name = m.group(0);
-                        } else if (m.group(0).startsWith("s")) {
-                            surname = m.group(0);
+                    String surnameTag = "";
+                    String stringsStr = strings.toString();
+                    while (matcher.find()) {
+                        if(!name.isEmpty() && !surname.isEmpty()){
+                            stringsStr = stringsStr.replaceAll(surnameTag+"\\s*?", "");
+                            stringsStr = stringsStr.replaceAll(name, name+" "+surname);
+                            name = "";
+                            surname = "";
+                        }
+                        if("name".equals(matcher.group(1))) {
+                            name = matcher.group(4);
+                        } else if ("surname".equals(matcher.group(1))) {
+                            surname = matcher.group(4);
+                            surnameTag = matcher.group(0);
                         }
                     }
-                    System.out.println(name+" "+surname);
+                    stringBuilder.append(stringsStr.replaceAll("\\s*$", "")).append("\n");
                     strings.setLength(0);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        if(!stringBuilder.isEmpty()) writeOutput();
+    }
+
+    private void writeOutput(){
+        System.out.println(stringBuilder);
+       /* try (FileOutputStream fileOutputStream = new FileOutputStream(urlOutput);
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fileOutputStream))) {
+            writer.write(stringBuilder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 
 }
